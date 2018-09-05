6e0943a80eda3b9b3646757eea9848905f9a1bc6
Rajat Gupta <rjtgupta84@gmail.com>
2018-09-05 15:18:27 +0530

    add the packages which have gpg problem
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- script.sh
+++ script.sh
@@ -13,4 +13,14 @@ sudo pacman -S pacaur
 # Installing the bare level packages which are needed
 
 pacaur -S base-devel git wget multilib-devel cmake svn clang
- 
+
+# The gpg key for the following packages isn't working as of now, so skip the gpg check. kanged from @akhilnarang 's for loop. :p
+
+for var in ncurses5-compat-libs lib32-ncurses5-compat-libs;
+    do
+        git clone https://aur.archlinux.org/$var
+        cd $var
+        makepkg -si --skippgpcheck
+        cd ..
+        rm -rf $var
+    done
