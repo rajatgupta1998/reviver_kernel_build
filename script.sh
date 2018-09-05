54b5ad93bea0e822270cb89a60f7044a93adf616
Rajat Gupta <rjtgupta84@gmail.com>
2018-09-05 14:30:25 +0530

    Install bare level packages
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- script.sh
+++ script.sh
@@ -9,3 +9,8 @@ sudo pacman -Syu
 # Let us use pacaur for installing packages from the AUR (Arch User Repository)
 
 sudo pacman -S pacaur
+
+# Installing the bare level packages which are needed
+
+pacaur -S base-devel git wget multilib-devel cmake svn clang
+ 
