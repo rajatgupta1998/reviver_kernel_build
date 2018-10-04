ef4bcd2f8d51062146c45cee74f9eaa89c735f36
Rajat Gupta <rjtgupta84@gmail.com>
2018-10-04 23:19:23 +0530

    let's git the shit

--- package-downloader.sh
+++ package-downloader.sh
@@ -6,13 +6,16 @@ echo "Updating your Arch system, please wait a while"
 
 sudo pacman -Syu
 
+# Let's git the shit
+sudo pacman -S git
+
 # Let us use pacaur for installing packages from the AUR (Arch User Repository)
 
 sudo pacman -S pacaur
 
 # Installing the bare level packages which are needed
 
-pacaur -S base-devel git wget multilib-devel cmake svn clang
+pacaur -S base-devel wget multilib-devel cmake svn clang
 
 # The gpg key for the following packages isn't working as of now, so skip the gpg check. kanged from @akhilnarang 's for loop. :p
 
