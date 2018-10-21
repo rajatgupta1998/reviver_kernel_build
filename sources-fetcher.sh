d2d5740b97d71e67758dc16e8b7cb3ec62853428
Rajat Gupta <rjtgupta84@gmail.com>
2018-10-21 15:15:10 +0530

    Download Google's arm eabi 4.8 toolchain and clone it to a path
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- sources-fetcher.sh
+++ sources-fetcher.sh
@@ -4,3 +4,10 @@ echo "Creating directories for kernel source and the compiler"
 
 mkdir -p source
 mkdir -p toolchain
+
+
+# Let us download the cross compiler
+
+echo "Downloading Google's ARM EABI 4.8 toolchain"
+
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8  toolchain/google-arm-eabi-4.8
