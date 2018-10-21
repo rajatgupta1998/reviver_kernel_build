9e302a268595920950caf9781869201875e36e78
Rajat Gupta <rjtgupta84@gmail.com>
2018-10-21 15:23:53 +0530

    Clone the main kernel source
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- sources-fetcher.sh
+++ sources-fetcher.sh
@@ -11,3 +11,7 @@ mkdir -p toolchain
 echo "Downloading Google's ARM EABI 4.8 toolchain"
 
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8  toolchain/google-arm-eabi-4.8
+
+# Get the main kernel source
+
+git clone https://github.com/rajatgupta1998/android_kernel_motorola_msm8952.git -b nougat-7.1.2-arm source/nougat-7.1.2-arm
